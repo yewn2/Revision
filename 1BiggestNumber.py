0b2ef004ab8e168e05a5f73f2f1ae9116706c89f
yewn2 <yewn2@middleton.school.nz>
2024-02-07 21:14:23 +1300

    Exercise 1

--- 1BiggestNumber.py
+++ 1BiggestNumber.py
@@ -1,22 +1,19 @@
 def biggest_number():
-    two = input("Enter two numbers, separated by a space; enter the first "
-                "number as '0' to cancel: ")
-    a, b = two.split(" ")
-    a = int(a)
-    b = int(b)
+    play_on = True
+    a = int(input("What is your first number? "))
+    b = int(input("What is your second number? "))
     equal_statement = "Numbers equal."
-    while a != 0:
-        a = int(a)
-        b = int(b)
+    while play_on:
         if a == b:
             print(equal_statement)
         elif a >= b:
-            print(a)
+            print(f"The higher number is {a}")
         elif a <= b:
-            print(b)
-        two = input("Enter two numbers, separated by a space; enter the first "
-                    "number as '0' to cancel: ")
-        a, b = two.split(" ")
+            print(f"The higher number is {b}")
+        a = int(input("What is your first number? "))
+        b = int(input("What is your second number? "))
+        if a == 0 or b == 0:
+            play_on = False
 
 
 biggest_number()
